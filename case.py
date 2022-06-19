fcaf7bf4f32a32af1d3fc1dbdfcd949574b62b12
Ives Gao (高亚洲) <ives.gao@ecloudrover.com>
2022-06-19 23:03:07 +0800

    mutli-account

--- case.py
+++ case.py
@@ -1,24 +1,27 @@
 import boto3
 
+accounts=[
+    'arn:aws:iam::224886979707:role/SupportECR',
+    'arn:aws:iam::366935765673:role/SupportECR'
+]
+
 def desribe_case(support_client):
     response = support_client.describe_cases()
     print(response)
 def listBucket(support_resources):
     for bucket in support_resources.buckets.all():
         print(bucket.name)
-def get_sts_token():
+def get_sts_token(account):
     sts_client = boto3.client('sts')
     role_session_name = "cross_cases_session"
     assumed_role_object=sts_client.assume_role(
-        RoleArn="arn:aws:iam::366935765673:role/SupportECR",
+        RoleArn=account,
         RoleSessionName=role_session_name
     )
     ACCESS_KEY    = assumed_role_object['Credentials']['AccessKeyId']
     SECRET_KEY    = assumed_role_object['Credentials']['SecretAccessKey']
     SESSION_TOKEN = assumed_role_object['Credentials']['SessionToken']
-    print(ACCESS_KEY)
-    print(SECRET_KEY)
-    print(SESSION_TOKEN)
+    
     
     support_resources=boto3.resource(
         's3',
@@ -28,8 +31,12 @@ def get_sts_token():
     )
     return support_resources
 def main():
-    support_resources=get_sts_token()
-    listBucket(support_resources=support_resources)
+    for account in accounts:
+        try:
+            support_resources=get_sts_token(account=account)
+            listBucket(support_resources=support_resources)
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     main()
