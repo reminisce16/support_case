db149c620468614454cbf3958aa1ea861792bfeb
Ives Gao (高亚洲) <ives.gao@ecloudrover.com>
2022-06-19 20:20:38 +0800

    simple

--- case.py
+++ case.py
@@ -3,6 +3,9 @@ import boto3
 def desribe_case(support_client):
     response = support_client.describe_cases()
     print(response)
+def listBucket(support_resources):
+    for bucket in support_resources.buckets.all():
+        print(bucket.name)
 def get_sts_token():
     sts_client = boto3.client('sts')
     role_session_name = "cross_cases_session"
@@ -13,17 +16,20 @@ def get_sts_token():
     ACCESS_KEY    = assumed_role_object['Credentials']['AccessKeyId']
     SECRET_KEY    = assumed_role_object['Credentials']['SecretAccessKey']
     SESSION_TOKEN = assumed_role_object['Credentials']['SessionToken']
+    print(ACCESS_KEY)
+    print(SECRET_KEY)
+    print(SESSION_TOKEN)
     
-    support_resources=boto3.client(
-        'Support',
+    support_resources=boto3.resource(
+        's3',
         aws_access_key_id=ACCESS_KEY,
         aws_secret_access_key=SECRET_KEY,
         aws_session_token=SESSION_TOKEN
     )
-    print(support_resources)
-    #return support_resources
+    return support_resources
 def main():
-    get_sts_token()
-    
-if __name__ == "__main__":
+    support_resources=get_sts_token()
+    listBucket(support_resources=support_resources)
+
+if __name__ == '__main__':
     main()
